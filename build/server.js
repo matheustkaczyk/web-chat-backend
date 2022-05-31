65ea38c8bf314757c99a0396ff33881b33a5277f
matheustkaczyk <matheus111904@hotmail.com>
2022-05-31 10:16:11 -0300

    feat: estruturando socket.io no server.ts

--- build/server.js
+++ build/server.js
@@ -4,4 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-app_1.default.listen(3000, () => console.log('Server is running on port 3000'));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const server = http_1.default.createServer(app_1.default);
+const io = new socket_io_1.Server(server);
+io.on('connection', () => {
+    console.log('New user connected');
+});
+server.listen(3000, () => console.log('Server is running on port 3000'));
--- server.ts
+++ server.ts
@@ -1,3 +1,12 @@
 import app from './app';
+import http from 'http';
+import { Server } from 'socket.io';
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', () => {  
+  console.log('New user connected');
+})
+
+server.listen(3000, () => console.log('Server is running on port 3000'));
