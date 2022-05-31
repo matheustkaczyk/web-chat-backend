cad85ef116bd9ed83734354b7e51d6ef462e01ad
matheustkaczyk <matheus111904@hotmail.com>
2022-05-31 16:11:40 -0300

    fix: corrigindo pequeno equivoco, socket.on message deveria estar dentro do connection

--- server.ts
+++ server.ts
@@ -5,8 +5,16 @@ import { Server } from 'socket.io';
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', () => {  
-  console.log('New user connected');
+io.on('connection', (socket) => {  
+  console.log('New user connected', socket.id);
+
+  socket.on('message', (msg) => {
+    console.log('message: ' + msg);
+  });
+})
+
+io.on('disconnect', () => {
+  console.log('User disconnected');
 })
 
 server.listen(3001, () => console.log('Server is running on port 3001'));
