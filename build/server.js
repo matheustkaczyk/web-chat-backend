9a65c36cc33c1d1e66b61ad400070c5070ed371d
matheustkaczyk <matheus111904@hotmail.com>
2022-06-07 20:39:06 -0300

    fix: adicionando script de build e start

--- build/server.js
+++ build/server.js
@@ -6,8 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const server = http_1.default.createServer(app_1.default);
 const io = new socket_io_1.Server(server);
+const PORT = process.env.PORT;
 io.on('connection', (socket) => {
     socket.broadcast.emit('message', `Novo usuário conectado - ${socket.id}`);
     socket.emit('message', `Seja bem vindo!`);
@@ -20,4 +23,4 @@ io.on('connection', (socket) => {
         io.to(object.room).emit('message', `${object.username} - ${object.message}`);
     });
 });
-server.listen(3001, () => console.log('Server is running on port 3001'));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
